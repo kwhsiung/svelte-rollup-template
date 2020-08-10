5d107e4f19740f080afa5717a52aa9137afcebe5
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-10 17:56:17 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  rules: {
+    // https://github.com/sveltejs/eslint-plugin-svelte3/issues/41#issuecomment-572503966
+    'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 2, maxEOF: 0 }]
+  }
+}
